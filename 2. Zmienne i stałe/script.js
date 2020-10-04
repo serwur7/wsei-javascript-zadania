f01d185799d3caf3460f94ea7b156c39a82e682a
serwur7 <49037456+serwur7@users.noreply.github.com>
2020-10-04 15:04:27 +0200

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,23 @@
+//Zadanie 1:
+
+
+
+
+
+//Zadanie 2:
+
+let num = 1,
+    num1 = 2,
+    sum = 0;
+
+sum = num + num1;
+
+console.log(sum);
+
+
+//Zadanie 3:
+
+let num;
+console.log(num);
+
+//Wynik- undefined, ponieważ zmienna nie ma zadeklarowanej wartości
