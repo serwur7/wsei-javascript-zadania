c023a9e21203ba9765922fe71f2415769793caf8
serwur7 <49037456+serwur7@users.noreply.github.com>
2020-10-18 16:23:37 +0200

    Update script.js

--- 7. Tablice/script.js
+++ 7. Tablice/script.js
@@ -54,3 +54,90 @@ tab.forEach(el => {
     }
 });
 
+//Zadanie 3
+
+const t2 = [1, 2, 3, 4, 5];
+
+//1
+
+const sum = t2.reduce((acc, number) => (acc + number));
+console.log(sum)
+
+//2
+
+const dif = t2.reduce((acc, number) => (acc - number));
+console.log(dif)
+
+//3
+
+const avg = sum / t2.length;
+console.log(avg);
+
+//4
+
+t2.forEach((el) => {
+	if (el % 2 === 0) {
+		console.log(el);
+	}
+});
+
+//5
+
+t2.forEach((el) => {
+	if (el % 2 !== 0) {
+		console.log(el);
+	}
+});
+
+//6
+
+const result = t2.sort((a,b) => a - b);
+console.log('max:', result[result.length - 1])
+
+
+//7
+
+t2.sort((a,b ) => b - a );
+console.log('min', result[result.length - 1])
+
+//8
+
+t2.reverse().forEach(el => console.log(el))
+
+//Zadanie 4
+
+const t2 = [1, 2, 3, 4, 5, 22, 33, 44];
+
+const func = tab => tab.reduce((a,b)=>a+b);
+
+console.log(func(t2));
+
+//Zadanie 5
+
+function A(tab){
+    const sum = tab.reduce((a,b)=>a+b);
+    const avg = sum / tab.length;
+    const result = tab.map(el => console.log(el * avg))
+}
+
+A(t2)
+
+//Zadanie 6
+
+const t2 = [1, 2, 3, 4, 5, 22, 33, 44];
+
+function x(tab){
+    const avg = tab.reduce((a,b)=> a+b) / tab.length ;
+    console.log(avg)
+}
+
+x(t2);
+
+//Zadanie 7
+
+
+
+
+
+
+
