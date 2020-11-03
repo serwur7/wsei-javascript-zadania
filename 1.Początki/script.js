dd56103abd25d70f0b280d7c46fd79910ded42c7
serwur7 <49037456+serwur7@users.noreply.github.com>
2020-11-04 00:50:52 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,18 @@
+// Zadanie 1
+
+console.log('Kamil Siedlak');
+
+//Zadanie 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+
+//Zadanie 3
+
+console.log("Hello World");
+console.log(Hello World);
+console.log("Hello World);
+
+//Pierwsze wyświetla komunikat Hello World, pozostałe dwa- błędy (brak ")
