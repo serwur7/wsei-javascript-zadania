dd1be6ead82b51718c84a8ff261fe9e771548bea
serwur7 <49037456+serwur7@users.noreply.github.com>
2020-11-04 01:09:37 +0100

    Update script.js

--- 4.Pętle i ify/script.js
+++ 4.Pętle i ify/script.js
@@ -58,7 +58,7 @@ for(let i = 0; i < n; i++){
 
 //Zadanie 7:
 
-for (let var i=1; i < 101; i++){
+for (i=1; i < 101; i++){
     if (i % 15 == 0) console.log("FizzBuzz");
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
